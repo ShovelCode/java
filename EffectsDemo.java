0f0ce3cd9e9bbeb14fcc77f008d7cb94e3eac1e1
ShovelCode <rchss@rocketmail.com>
2020-09-13 00:45:26 -0500

    Create EffectsDemo.java

---
+++ EffectsDemo.java
@@ -0,0 +1,9 @@
+public class EffectsDemo extends Application {
+    public static void main(String[]args) {
+	Application.launch(args);
+    }
+    @Override
+    public void start(Stage stage){
+	//code here
+    }
+}
