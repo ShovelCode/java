a780cebc836efdb7f6efc2371c229c27a99d3d90
Christopher Ray <vde435@gmail.com>
2021-01-01 00:38:23 -0600

    Create CurriedCalc.java

---
+++ CurriedCalc.java
@@ -0,0 +1,40 @@
+interface Function<T, R>
+{
+   R apply(T t);
+}
+public class CurriedCalc
+{
+   public static void main(String[] args)
+   {
+      System.out.println(calc(1).apply(2).apply(3).apply(4));
+   }
+   static Function<Integer, Function<Integer, Function<Integer, Integer>>> 
+      calc(final Integer a)
+   {
+      return new Function<Integer, 
+                          Function<Integer, Function<Integer, Integer>>>()
+             {
+                @Override
+                public Function<Integer, Function<Integer, Integer>> 
+                   apply(final Integer b)
+                {
+                   return new Function<Integer, Function<Integer, Integer>>()
+                          {
+                             @Override
+                             public Function<Integer, Integer> 
+                                apply(final Integer c)
+                             {
+                                return new Function<Integer, Integer>()
+                                {
+                                   @Override
+                                   public Integer apply(Integer d)
+                                   {
+                                      return (a + b) * (c + d);
+                                   }
+                                };
+                             }
+                          };
+                }
+             };
+   }
+}
