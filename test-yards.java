7f4bf54101687de2878ddab9fdf9a1fe4f62056f
Christopher Ray <vde435@gmail.com>
2021-01-20 23:03:04 -0600

    Create test-yards.java

---
+++ test-yards.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ 
+ 
+public class YardConversion {
+ 
+    public static void main(String[] args) {
+        String input = "";
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Enter yards: ");
+        input = s.next();
+        System.out.println(input);
+        
+        
+        
+ 
+    }
+ 
+}
