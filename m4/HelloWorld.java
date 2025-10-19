b62f7c73f955b612482bba4793f348b6dda87e02
Christopher Ray <vde435@gmail.com>
2025-10-19 00:51:07 -0500

    Add HelloWorld.java with basic Java program

---
+++ m4/HelloWorld.java
@@ -0,0 +1,9 @@
+//The old tried-and-true still works, but requires a special option when compiling.
+//JDK 21
+import java.lang.*;
+
+public class HelloWorld {
+public static void main(String[]args){
+  System.out.println("Hello world!");
+}
+}
